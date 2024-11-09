8e0d63a43c1867c7c9806e3601061cd45dd20dc1
scarlet <scarryaa@gmail.com>
2024-11-08 19:38:26 -0500

    fixed search highlights not updating/re-calculating when opening new files

--- lib/screens/editor_screen.dart
+++ lib/screens/editor_screen.dart
@@ -120,6 +120,8 @@ class _EditorScreenState extends State<EditorScreen> {
         activeEditorIndex = _editors.length - 1;
         _editors[activeEditorIndex].openFile(content);
       });
+
+      searchService.onSearchTermChanged(searchService.searchTerm, activeEditor);
     }
   }
 
