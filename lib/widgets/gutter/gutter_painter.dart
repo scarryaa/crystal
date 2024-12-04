5def301e8a1ab9c13cbb94405672584be49e2e55
scarlet <scarryaa@gmail.com>
2024-12-04 10:25:57 -0500

    improved performance of gutter painter

--- lib/widgets/gutter/gutter_painter.dart
+++ lib/widgets/gutter/gutter_painter.dart
@@ -1,5 +1,4 @@
 import 'dart:math';
-
 import 'package:crystal/core/editor/editor_core.dart';
 import 'package:flutter/material.dart';
 
@@ -10,11 +9,34 @@ class GutterPainter extends CustomPainter {
   final int firstVisibleLine;
   final int lastVisibleLine;
 
+  late final double gutterWidth;
+  late final List<TextSpan> _cachedSpans;
+
   GutterPainter({
     required this.core,
     required this.firstVisibleLine,
     required this.lastVisibleLine,
-  });
+  }) {
+    final totalLines = core.lines.length;
+    final width = totalLines.toString().length;
+    gutterWidth = (width * core.config.characterWidth) + (textPadding * 2);
+
+    _initializeCache(totalLines, width);
+  }
+
+  void _initializeCache(int totalLines, int width) {
+    _cachedSpans = List.generate(
+      totalLines,
+      (i) => TextSpan(
+        style: TextStyle(
+          fontFamily: core.config.fontFamily,
+          fontSize: core.config.fontSize,
+          color: core.config.gutterTextcolor,
+        ),
+        text: '${(i + 1).toString().padLeft(width)}\n',
+      ),
+    );
+  }
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -23,35 +45,37 @@ class GutterPainter extends CustomPainter {
   }
 
   void drawBackground(Canvas canvas, Size size) {
-    canvas.drawRect(Rect.fromLTWH(0, 0, size.width, size.height),
-        Paint()..color = Colors.white);
+    canvas.drawRect(
+      Rect.fromLTWH(0, 0, size.width, size.height),
+      Paint()..color = Colors.white,
+    );
   }
 
   void drawLines(Canvas canvas, Size size) {
-    final totalLines = core.lines.length;
-    final width = totalLines.toString().length;
-
-    final gutterWidth =
-        (width * core.config.characterWidth) + (textPadding * 2);
+    final start = firstVisibleLine;
+    final end = min(core.lines.length, lastVisibleLine + 1);
 
     textPainter.text = TextSpan(
-        children: List.generate(
-            min(totalLines, firstVisibleLine + lastVisibleLine),
-            (i) => TextSpan(
-                style: TextStyle(
-                  fontFamily: core.config.fontFamily,
-                  fontSize: core.config.fontSize,
-                  color: core.config.gutterTextcolor,
-                ),
-                text: '${(i + 1).toString().padLeft(width)}\n')));
+      children: _cachedSpans.sublist(start, end),
+    );
 
-    textPainter.layout();
+    textPainter.layout(
+      maxWidth: size.width,
+      minWidth: 0,
+    );
+
+    final verticalOffset = (firstVisibleLine * core.config.lineHeight);
+
     textPainter.paint(
-        canvas, Offset(size.width - gutterWidth + textPadding, 0));
+      canvas,
+      Offset(size.width - gutterWidth + textPadding, verticalOffset),
+    );
   }
 
   @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return true;
+  bool shouldRepaint(GutterPainter oldDelegate) {
+    return oldDelegate.firstVisibleLine != firstVisibleLine ||
+        oldDelegate.lastVisibleLine != lastVisibleLine ||
+        oldDelegate.core != core;
   }
 }
